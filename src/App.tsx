2dadd89b2593a3180eff05b96e15c91a9b17c96a
alexandre-kakal-akarah <alexandre.ls.kakal@gmail.com>
2025-07-07 14:15:35 +0200

    feat(model): - add rick model - set default animation to idle

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Map from "./components/Map";
 import "./App.css";
+import { Rick } from "./components/Rick";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
       <Map />
+      <Rick />
       <OrbitControls />
     </Canvas>
   );
